ae3a8c4d92b412b7ce35da24ec9a9311fbdce3df
elrrowwe <248607@edu.p.lodz.pl>
2024-01-10 15:52:35 +0100

    v1 of the network

--- generator.py
+++ generator.py
@@ -1,40 +1,56 @@
 """
 This file contains the generator part of the GAN model.
+
+The generator, in this context, accepts a 1 x z_dim (see the constructor) vector of noise drawn from the normal distribution
+and gradually upsamples it to the desired size, while also transforming it into a realistic image. 
 """
 
 import torch
 import torch.nn as nn
 
 class Generator(nn.Module):
-    def __init__(self, inp_dim: tuple[int], n_channels: int=1, n_conv_kernels: int=3, device='cpu'):
+    def __init__(self, z_dim: int, lin1_hw: int=5, lin1_channels: int=256, output_channels: int=1, device: str='cpu'):
         """
         The constructor of the Generator part of the network.
 
-        inp_dim: the dimensions of the input
-        n_channels: the number of channels in the input. defaults to 1
-        n_conv_kernels: the number of convolutional kernels in the first layer of the network. defaults to 3  
+        z_dim: the dimension of the input noise vector
+        lin1_hw: the height, width of the feature map produced by the first, dense layer. defaults to 5
+        lin1_channels: the number of channels in the output of the first, dense layer. defaults to 128
+        output_channels: the number of channels in the output of the network. defaults to 1 
         device: the device to send the calculations to. defaults to 'cpu' 
         """
         super(Generator, self).__init__()  
 
+        self.z_dim = z_dim
+        self.lin1_channels = lin1_channels
+        self.lin1_hw = lin1_hw
+        self.output_channels = output_channels
+        self.device = device
+
+        #converting the noise vector into a tensor
+        self.lin1 = nn.Linear(self.z_dim, self.lin1_hw * self.lin1_hw * self.lin1_channels, device=device), #a lin1_hw x lin1_hw spatial resolution, lin1_channels channels. the resolution may be in the lower range for efficiency 
+
         self.G = nn.Sequential(
-            nn.ConvTranspose2d(n_channels, n_conv_kernels, 2, device=device),
-
+            #the upsampling sequence 
+            nn.ConvTranspose2d(self.lin1_channels, self.lin1_channels / 2, kernel_size=4, stride=2, device=self.device),
             nn.LeakyReLU(0.1, True),
 
-            nn.Dropout(inplace=True),
-
-            nn.Linear(n_conv_kernels, n_conv_kernels, device=device),
+            nn.ConvTranspose2d(self.lin1_channels / 2, self.lin1_channels / 4, kernel_size=4, stride=2, device=self.device),
             nn.LeakyReLU(0.1, True),
 
-            nn.ConvTranspose2d(n_conv_kernels, n_channels),
+            nn.ConvTranspose2d(self.lin1_channels / 4, output_channels, kernel_size=4, stride=2, device=self.device),
+
             nn.Tanh()
         )
 
-    def forward(self, X: torch.Tensor):
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
         """
         The forward pass of the network.
 
         returns an image.
         """
-        return self.G(X) 
+        z = self.lin1(z)
+
+        z = z.view(-1, 256, 4, 4)
+        
+        return self.G(z) 
